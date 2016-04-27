64be3900ac558edda017e05f287ec9901253a6b5
Alex Brizius <abrizius@nd.edu>
2016-04-27 09:32:00 -0400

    changed caps, added comments

--- src/board.cpp
+++ src/board.cpp
@@ -4,6 +4,7 @@
 /*Macros*/
 #include "stdlib.h"
 #include <locale>
+#include <ctype.h>
 
 Board::Board(){	
 	dim = 8;
@@ -166,8 +167,6 @@ void Board::initPieces(){
     queenBoard.push_back(tempVector);
     tempVector.clear();
 
-
-
 }
 
 
@@ -180,9 +179,9 @@ void Board::display(){
 		for( int j = 0; j < dim; j++){
 			cout << "|";
 			if (isupper(chessBoard[j].at(i).getChar()))
-				cout << "\033[0;36m" << chessBoard[j].at(i).getChar() << "\033[0m";
+				cout << "\033[0;36m" << char(toupper(chessBoard[j].at(i).getChar())) << "\033[0m";
 			else if (islower(chessBoard[j].at(i).getChar()))
-				cout << "\033[1;31m" << chessBoard[j].at(i).getChar() << "\033[0m";
+				cout << "\033[1;31m" << char(toupper(chessBoard[j].at(i).getChar())) << "\033[0m";
 			else
 				cout << chessBoard[j].at(i).getChar();	
 			cout << "|";
